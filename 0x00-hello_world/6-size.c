47f7620905e3d5ae178e8c1f3e8fb808224bb19f
Xray15633 <jacksongerry00@gmail.com>
2023-02-17 06:14:49 -0800

    cgit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
- * main -   program that prints the size of various types
- * on the computer it is compiled and run on
- * Return 0 (Success)
+ * main - A program that prints the size of various types
+ * Return  0 (Success)
  */
 int main(void)
 {
