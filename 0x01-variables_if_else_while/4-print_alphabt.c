990e48f33a868646209c2505f2d2808ed8d73e81
Xray15633 <jacksongerry00@gmail.com>
2023-02-17 16:18:09 -0800

    flowe

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ *Print all the letters except q and e
+ * return: Always 0(Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
