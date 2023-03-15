d40ccc4def1450437f63e0b6a1bc78c9ee9cec03
Xray15633 <jacksongerry00@gmail.com>
2023-03-16 02:48:04 +0300

    ar

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include "main.h"	
+#include "main.h"
 /**
- * main - prints all arguments it receives
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
- */
+* main - prints all arguments it receives
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: Always 0 (Success)
+*/
 int main(int argc, char *argv[])
 {
 	int i;
@@ -15,5 +15,6 @@ int main(int argc, char *argv[])
 	{
 		printf("%s\n", argv[i]);
 	}
-	return (0);
+
+return (0);
 }
